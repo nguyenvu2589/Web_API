5ea5df194771d9bdfe1efcd50fd2012cb5884081
nguyenvu2589 <nguyenvu2589@gmail.com>
2017-02-25 19:10:35 -0700

    Add files via upload

--- HW2/api/controllers/car.js
+++ HW2/api/controllers/car.js
@@ -12,28 +12,44 @@ module.exports = {
 };
 // GET /car
 function getAll(req, res, next) {
-    res.json("You just call GET function");
+    res.json("You just call GET function. And no parameter");
 }
 function add(req, res,next){
+    if (req.body){
     res.setHeader('Content-Type', 'application/json');
     res.json({success:1, description: "Successfully add new car", "saved info": req.body });
-    
+    } else {
+        res.json({success:0, description:" FAil to request info"})
+        res.status(204).send();
+    }
 }
 function getOne(req, res, next) {
+    if (id){
     var  id = req.swagger.params.id.value;
     res.json("The car id you entered is "+ id );
-    
+    } else {
+        res.json({success:0, description:"ID was not in parameter"});
+        res.status(204).send();
+    }
 }
 function update(req, res, next) {
+    if (req.body){
     res.setHeader('Content-Type', 'application/json');
     res.json({success:1, description: "Successfully update car's info", "saved info": req.body });
-    
+    } else {
+        res.json({success:0, description:" FAil to request info"})
+        res.status(204).send();
+    }
 
 }
 
 function delCar(req, res, next) {
+    if (id){
     var  id = req.swagger.params.id.value;
     res.json({success:1, description: "You just deleted car :" + id });
-    
+    } else {
+        res.json({success:0, description:"ID was not in parameter"});
+        res.status(204).send();
+    }
 }
 
